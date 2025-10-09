afccc7887e5932f4a971c728956662b11148f0da
Dev-Expertise-TS <dev.expertise.ts@gmail.com>
2025-10-09 12:59:54 +0900

    하단 네비게이션 메뉴 위치 및 텍스트 변경경

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -18,11 +18,11 @@ export interface NavigationItem {
 export const navigationItems: NavigationItem[] = [
   { href: "/", label: "홈", icon: Home, mobileOnly: true },
   { href: "/about", label: "셀렉트 소개" },
-  { href: "/promotion", label: "프로모션", mobileLabel: "프로모션", icon: Tag },
   { href: "/brand", label: "브랜드 & 프로그램", mobileLabel: "브랜드", icon: Building2 },
+  { href: "/promotion", label: "프로모션", mobileLabel: "프로모션", icon: Tag },
   // 신규 모바일 전용 메뉴
   { href: "http://pf.kakao.com/_cxmxgNG/chat", label: "예약상담", mobileLabel: "예약상담", icon: MessageCircle, mobileOnly: true },
-  { href: "/hotel", label: "호텔 & 리조트 전체보기", mobileLabel: "전체호텔", icon: Hotel },
+  { href: "/hotel", label: "호텔 & 리조트 전체보기", mobileLabel: "호텔검색", icon: Hotel },
   { href: "/blog", label: "아티클", mobileLabel: "아티클", icon: FileText },
   { href: "/support", label: "투어비스", mobileLabel: "투어비스", icon: (TourvisIcon as unknown as LucideIcon), mobileOnly: true },
 ]
